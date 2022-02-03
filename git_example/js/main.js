123020c61da10e1741d768744222ef00ea9c37c7
marwa khemiri <marwa.khemiri17@gmail.com>
2022-02-03 16:13:36 +0100

    delate git example

--- git_example/js/main.js
+++
@@ -1,8 +0,0 @@
- Vue.createApp({
-     data() {
-         return {
-             product: "iPhone 13",
-             description: "iPhone 13 is the latest iPhone in the iPhone series. It is powered by a 2.84-GHz dual-core processor and has a camera system that can take up to 8 photos in a single burst.",
-         }
-     }
- }).mount('#app');
